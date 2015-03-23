a56341690f574dc8f068cdf8c0c3e81073f2d84b
Lori Kaufman <lori.kaufman@yale.edu>
2015-03-22 21:05:06 -0400

    Changed n to i and nondimensionalized x and y

--- part_b.m
+++ part_b.m
@@ -1,31 +1,31 @@
 %Perform summation given in problem to calculate temperature 
 %at a given co-ordinate
 
-function[val] = summation(x, y, n, q)
-	val = 0;
-	for i in 1:n
-		val = val + 2*q*((-1)^(n+1)+1)*sin(n*pi*x)*sinh(n*pi*y)/(n^2*pi^2*cosh(n*pi));
-	end
-end
-
 function[] = part_b()
 	%dimensions of the plate
-	x = 20;
-	y = 20;
+	w = 20;
+	l = 20;
 	%number of steps to do in the summation
 	n = 10;
 	%heat flow constant
 	q = .05;
 
 	%initialize temperature matrix
-	T = zeros(x,y);
+	T = zeros(w,l);
 	%fill in the values for the temperature matrix
-	for i in 1:x
-		for j in 1:y
-			T(i,j) = summation(i, j, n, q)
+	for i = 1:w
+		for j = 1:l
+			T(i,j) = summation(i, j, n, q, w, l);
 		end
 	end
 
 	%display T
-	T
+	imagesc(T);
+end
+
+function[val] = summation(x, y, n, q, w, l)
+	val = 0;
+	for i = 1:n
+		val = val + 2*q*((-1)^(i+1)+1)*sin(i*pi*x/w)*sinh(i*pi*y/l)/(i^2*pi^2*cosh(i*pi));
+	end
 end
